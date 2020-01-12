3bfd75508be6e7625ec6e18ea516e87d0bd94a54
ahmadAsmi <ahmadasmi95.ite@gmail.com>
2020-01-12 16:48:55 +0200

    Ahmad 1-12-2020

--- ArchiveProject2019/ViewModel/Informations.cs
+++ ArchiveProject2019/ViewModel/Informations.cs
@@ -1,4 +1,5 @@
 ﻿using ArchiveProject2019.Models;
+using ArchiveProject2019.Resources;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -11,39 +12,45 @@ namespace ArchiveProject2019.ViewModel
     public class Informations
     {
 
-
         public bool IsMasterRole { set; get; }
-        [Display(Name = "عدد   وثائقي ")]
+        [Display(Name = "MyDocumentCount", ResourceType = typeof(main_lang))]
 
         public int MyTotalDocument { set; get; }
 
-        [Display(Name = "عدد مجموعاتي ")]
+        [Display(Name = "MyGroupsCount", ResourceType = typeof(main_lang))]
+
 
         public int MyGroupsCount { set; get; }
+        [Display(Name = "LastCreateDocumentDate", ResourceType = typeof(main_lang))]
 
-        [Display(Name = "آخر تاريخ إضافة وثيقة")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public string LastMyDocumentCreate { set; get; }
+        [Display(Name = "TotalDepartmentsCount", ResourceType = typeof(main_lang))]
 
-        [Display(Name = " عدد الأقسام الكلية")]
-   
+
         public int DepartmentsCount { set; get; }
 
 
 
+        [Display(Name = "DocumentsKindCount", ResourceType = typeof(main_lang))]
 
-        [Display(Name = "عدد  أنواع الوثائق ")]
 
         public int TotalDocumentKindCount { set; get; }
-        [Display(Name = "آخر تاريخ إضافة ")]
+
+
+
+        [Display(Name = "LasteCreateDate", ResourceType = typeof(main_lang))]
+
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy-HH:mm:ss}")]
         public string LastDocumentKindCreate { set; get; }
 
-        [Display(Name = "آخر تاريخ تحديث ")]
+        [Display(Name = "LastUpdateDate", ResourceType = typeof(main_lang))]
+
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy-HH:mm:ss}")]
         public string LastDocumentKindUpdate { set; get; }
 
-        [Display(Name = "أكثر نوع مستخدم")]
+
+        [Display(Name = "HightDocumentKindUsing", ResourceType = typeof(main_lang))]
 
         public string HightDocumentKindUsing { set; get; }
 
@@ -56,38 +63,44 @@ namespace ArchiveProject2019.ViewModel
 
 
 
-        [Display(Name = "عدد الأقسام الرئيسية")]
+        [Display(Name = "MainDepartmentsCount", ResourceType = typeof(main_lang))]
+
         public int MainDepartmentCount { set; get; }
-        [Display(Name = "عدد الأقسام الكلية")]
+        [Display(Name = "AllDepartmentsCount", ResourceType = typeof(main_lang))]
+
         public int AllDepartmentsCount { set; get; }
-        [Display(Name = "آخر تاريخ إضافة ")]
+        [Display(Name = "LasteCreateDate", ResourceType = typeof(main_lang))]
+
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy-HH:mm:ss}")]
         public string LastDateDepartmentCreate { set; get; }
 
 
-        [Display(Name = "آخر تاريخ تحديث ")]
+        [Display(Name = "LastUpdateDate", ResourceType = typeof(main_lang))]
+
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy-HH:mm:ss}")]
         public string LastDateDepartmentUpdate { set; get; }
 
 
 
-        [Display(Name = "عدد  المجموعات الكلية")]
-  
+        [Display(Name = "AllGroupsCount", ResourceType = typeof(main_lang))]
+
+
         public int AllGroupsCount { set; get; }
-        [Display(Name = "آخر تاريخ إضافة ")]
+        [Display(Name = "LasteCreateDate", ResourceType = typeof(main_lang))]
+
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy-HH:mm:ss}")]
         public string LastGroupCreate { set; get; }
 
 
 
-        [Display(Name = "آخر تاريخ تحديث ")]
+        [Display(Name = "LastUpdateDate", ResourceType = typeof(main_lang))]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy-HH:mm:ss}")]
         public string LastGroupUpdate { set; get; }
-        [Display(Name = "عدد   المستخدمين في المجموعات")]
+        [Display(Name = "UsersCountInGroups", ResourceType = typeof(main_lang))]
 
         public int TotalUserInGroup { set; get; }
 
-        [Display(Name = "أكثر مجموعة للمستخدمين ")]
+        [Display(Name = "HightGroupUsers", ResourceType = typeof(main_lang))]
 
         public string HightGroupUsers { set; get; }
 
@@ -99,11 +112,13 @@ namespace ArchiveProject2019.ViewModel
         [Display(Name = "عدد  المستخدمين الكلي ")]
 
         public int TotalUserCount { set; get; }
-        [Display(Name = "آخر تاريخ إضافة ")]
+        [Display(Name = "LasteCreateDate", ResourceType = typeof(main_lang))]
+
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy-HH:mm:ss}")]
         public string LastUserCreate { set; get; }
 
-        [Display(Name = "آخر تاريخ تحديث ")]
+        [Display(Name = "LastUpdateDate", ResourceType = typeof(main_lang))]
+
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy-HH:mm:ss}")]
         public string LastUserUpdate { set; get; }
 
@@ -111,86 +126,102 @@ namespace ArchiveProject2019.ViewModel
 
 
 
-        [Display(Name = "عدد  النماذج الكلية ")]
+        [Display(Name = "AllFormsCount", ResourceType = typeof(main_lang))]
+
 
         public int TotalFormsCount { set; get; }
-        [Display(Name = "آخر تاريخ إضافة ")]
+        [Display(Name = "LasteCreateDate", ResourceType = typeof(main_lang))]
+
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy-HH:mm:ss}")]
         public string LastFormCreate { set; get; }
 
-        [Display(Name = "آخر تاريخ تحديث ")]
+        [Display(Name = "LastUpdateDate", ResourceType = typeof(main_lang))]
+
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy-HH:mm:ss}")]
         public string LastFormUpdate { set; get; }
 
-        [Display(Name = "أكثر نموذج مستخدم")]
-       
+        [Display(Name = "HightFormUsing", ResourceType = typeof(main_lang))]
+
         public string HightFormUsing { set; get; }
 
 
 
 
 
-        [Display(Name = "عدد  الوثائق الكلية ")]
+        [Display(Name = "TotalDocumentCount", ResourceType = typeof(main_lang))]
 
         public int TotalDocumentCount { set; get; }
-        [Display(Name = "آخر تاريخ إضافة ")]
+        [Display(Name = "LasteCreateDate", ResourceType = typeof(main_lang))]
+
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy-HH:mm:ss}")]
         public string LastDocumentCreate { set; get; }
 
 
 
-        [Display(Name = "عدد   أنواع البريد ")]
+        [Display(Name = "TotalMailsCount", ResourceType = typeof(main_lang))]
+
 
         public int TotalMailsCount { set; get; }
-        [Display(Name = "آخر تاريخ إضافة ")]
+        [Display(Name = "LasteCreateDate", ResourceType = typeof(main_lang))]
+
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy-HH:mm:ss}")]
         public string LastMailsCreate { set; get; }
 
 
-        [Display(Name = "آخر تاريخ تحديث ")]
+        [Display(Name = "LastUpdateDate", ResourceType = typeof(main_lang))]
+
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy-HH:mm:ss}")]
         public string LastMailsUpdate { set; get; }
 
-        [Display(Name = "أكثر نوع بريد مستخدم")]
+        [Display(Name = "HightMailUsing", ResourceType = typeof(main_lang))]
+
 
         public string HightMailUsing { set; get; }
 
 
 
-        //Personal Informations:
-        [Display(Name = "الإسم الثلاثي")]
+        [Display(Name = "UserFullName", ResourceType = typeof(main_lang))]
+
         public string FullName { set; get; }
 
-        [Display(Name = "اسم المستخدم")]
+        [Display(Name = "UserUserName", ResourceType = typeof(main_lang))]
+
         public string UserName { set; get; }
 
-        [Display(Name = "البريد الإلكتروني")]
+        [Display(Name = "UserEmail", ResourceType = typeof(main_lang))]
+
         public string Email { set; get; }
 
 
-        [Display(Name = "الجنس")]
+        [Display(Name = "UserGender", ResourceType = typeof(main_lang))]
+
         public string Gender { set; get; }
 
-        [Display(Name = "آخر تاريخ تحديث ")]
+        [Display(Name = "UpdatedAt", ResourceType = typeof(main_lang))]
+
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy-HH:mm:ss}")]
 
         public string UserUpdate { set; get; }
 
 
-        [Display(Name = "الدور")]
+        [Display(Name = "UserRole", ResourceType = typeof(main_lang))]
+
         public string RoleName { set; get; }
 
 
-        [Display(Name = "تاريخ الإنشاء")]
+        [Display(Name = "CreatedAt", ResourceType = typeof(main_lang))]
+
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy-HH:mm:ss}")]
 
         public string UserCreateAt { set; get; }
 
 
-        [Display(Name = " القسم الخاص")]
+        [Display(Name = "UserDepartment", ResourceType = typeof(main_lang))]
+
         public string DepartmentName { set; get; }
 
-        [Display(Name = "المسمى الوظيفي")]
+        [Display(Name = "UserJopTitle", ResourceType = typeof(main_lang))]
+
         public string JobTitle { set; get; }
 
 
